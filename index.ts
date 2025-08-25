335dc8436c29cc2ef09dd64a11877633688bcbdf
retrozinndev <joaovodias@gmail.com>
2025-08-25 12:50:37 -0300

    :wrench: chore(index): add index for better imports

---
+++ index.ts
@@ -0,0 +1,14 @@
+import Plugin from "./src/plugin";
+import Album from "./src/_album";
+import Song from "./src/_song";
+import Artist from "./src/_artist";
+import Vibe from "./src/vibe";
+
+
+export {
+    Plugin,
+    Album,
+    Song,
+    Artist,
+    Vibe as VibeAPI
+}
