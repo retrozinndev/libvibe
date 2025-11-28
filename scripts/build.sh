2f7437f6bae2b95d433060403917e045ba3fc518
retrozinndev <joaovodias@gmail.com>
2025-11-28 16:55:53 -0300

    :wrench: chore(package, scripts): add more package data, remove unused scripts

--- scripts/build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-dist=${@:-./dist}
-
-if [[ -d "$dist" ]]; then
-    sh scripts/clean.sh
-fi
-
-mkdir -p $dist
--- scripts/clean.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-dist=${@:-./dist}
-
-echo "[info] cleaning dist"
-rm -rf $dist
