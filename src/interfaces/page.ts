0557cf5b73810e5b2e64d62170796902f7ff5011
retrozinndev <joaovodias@gmail.com>
2026-01-17 13:44:14 -0300

    :wrench: chore(interfaces/page): allow content to be a custom widget

--- src/interfaces/page.ts
+++ src/interfaces/page.ts
@@ -5,23 +5,22 @@ import { Album, Artist, Playlist, Song } from "../objects";
 import { Accessor } from "gnim";
 
 
-export type PageType = Artist|Song|Album|Playlist|undefined;
+export type PageType = Artist|Song|Album|Playlist|Gtk.Widget;
 
-export type Props = {
+type BaseProps = {
     title?: string|Accessor<string>;
     id?: any;
 };
-
-export type PageProps<T extends PageType = undefined> = T extends NonNullable<PageType> ?
-    Props & {
-        content: NonNullable<T>;
-        sections?: Array<Section>|Accessor<Array<Section>>;
-        buttons?: Array<IconButton & LabelButton>|Accessor<Array<IconButton&LabelButton>>;
-    }
-: Props
+export type PageProps<T extends PageType> = T extends Gtk.Widget ?
+    BaseProps & { content: Gtk.Widget; }
+: BaseProps & {
+    content: NonNullable<T>;
+    sections?: Array<Section>|Accessor<Array<Section>>;
+    buttons?: Array<IconButton & LabelButton>|Accessor<Array<IconButton&LabelButton>>;
+}
 
 export interface Page<
-    T extends PageType = undefined
+    T extends PageType = Gtk.Widget
 > extends Adw.Bin {
     readonly id: any;
     /** page title. @default "New page"(if no content is defined; else it's based on the content) */
