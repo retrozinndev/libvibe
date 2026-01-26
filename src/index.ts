a239c941e2ed9a6efe75fb557b7b2818e6b3f957
retrozinndev <joaovodias@gmail.com>
2026-01-25 21:22:22 -0300

    :boom: fix(vibe): wrong `DetailedButton` type

--- src/index.ts
+++ src/index.ts
@@ -24,7 +24,7 @@ export type LabelButton = {
     onClicked?: () => void;
 };
 
-export type DetailedButton = IconButton|LabelButton;
+export type DetailedButton = IconButton&LabelButton;
 
 export type Section = {
     title: string;
